ccb33a40ae1bf95117b8b00773b2c16f76924cda
g00413670@atu.ie <g00413670@atu.ie>
2024-02-21 11:21:00 +0000

    lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AComponent } from './a/a.component'; 
+import { BComponent } from './b/b.component';
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-directive',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AComponent, BComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title:string = 'my-app';
+  name:string = "Titi";
+  age:number = 19;
 }
